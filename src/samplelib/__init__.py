623d7083103802981da06261fb77f23653b1b020
Shin Saito <shinsa@jp.ibm.com>
2021-06-12 16:53:56 +0900

    update directory structure

--- src/samplelib/__init__.py
+++ src/samplelib/__init__.py
@@ -1,5 +1,7 @@
 from pandas import DataFrame
 
+__version__ = "0.0.0"
+
 
 def foo():
     return DataFrame([[1, 2, 3], [4, 5, 6]], columns=list('abc'))
--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,11 @@
-from samplelib import foo
+from samplelib import foo, __version__
 
 
 def test_foo():
     df = foo()
     assert list(df.columns) == ['a', 'b', 'c']
+
+
+def test_version():
+    print(__version__)
+    assert __version__ == '0.0.0'
